0436b2dfa772e2fbe969cd994b6524c07b09b01e
Gorkem Ozkaya <gorkemozkaya@gmail.com>
2019-07-01 04:20:51 +0000

    modified train.sh

--- train.sh
+++ train.sh
@@ -7,17 +7,19 @@ STORAGE_BUCKET=gs://gorkemozkaya
 DATA_DIR=$STORAGE_BUCKET/data_v2
 TMP_DIR=/tmp/tmp
 TRAIN_DIR=$STORAGE_BUCKET/train_v2
-USR_DIR=/Users/gorkemozkaya/Projects/NMT/nmt-en-tr/t2t/problems/nmt-en-tr
+USR_DIR=/home/gorkemozkaya/nmt-en-tr/t2t/problems/nmt-en-tr
 
 mkdir -p $TMP_DIR 
 
 t2t-trainer \
   --t2t_usr_dir=$USR_DIR \
-  --generate_data \
-  --data_dir=/tmp/t2t_data \
-  --output_dir=/tmp/t2t_output \
-  --problem=sort_words_according_to_length_random \
+  --data_dir=$DATA_DIR \
+  --output_dir=$TRAIN_DIR \
+  --problem=$PROBLEM \
   --model=$MODEL \
   --hparams_set=$HPARAMS \
   --train_steps=250000 \
-  --eval_steps=25000
+  --eval_steps=25000 \
+  --use_tpu=True \
+  --tpu_name=gorkemozkaya \
+  --keep_checkpoint_max=20
